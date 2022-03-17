19709718aa0e36474b3db27a809e00c91e2b53b0
JacopoUboldi <uboldi.jacopo@iisgalvanimi.edu.it>
2022-03-17 09:01:20 +0000

    finito"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Empty-Project';
+
+  list:string [] = []
+  testo1:string = "Button1";
+  testo2:string = "Button2";
+  testo3:string = "Button3";
+  testo4:string = "Button4";
+  ultimo:string = ""
+  onClick1(){
+    this.list.push(this.testo1)
+  }
+
+  onClick2(){
+    this.list.push(this.testo2)
+  }
+
+  onClick3(){
+    this.list.push(this.testo3)
+  }
+
+  onClick4(){
+    this.list.push(this.testo4)
+  }
+
+
+
+
+
+
+
+
+
+
+
 }
